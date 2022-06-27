2fc501240c8665ac40a38915843171920f1e09a4
teamclouday <teamclouday@gmail.com>
2022-06-26 22:55:28 -0700

    add GL ext structures

--- Base/GLStructs.hpp
+++ Base/GLStructs.hpp
@@ -1,1 +1,158 @@
 #pragma once
+#include <GL/glew.h>
+
+#include <vector>
+
+namespace RenderIt
+{
+
+struct SVAO
+{
+    SVAO(size_t size = 1)
+    {
+        IDs.resize(size);
+        glGenVertexArrays(static_cast<GLsizei>(size), IDs.data());
+    }
+    ~SVAO()
+    {
+        glDeleteVertexArrays(static_cast<GLsizei>(IDs.size()), IDs.data());
+    }
+    void Bind(size_t idx = 0)
+    {
+        if (idx < IDs.size())
+            glBindVertexArray(IDs[idx]);
+    }
+    void UnBind()
+    {
+        glBindVertexArray(0);
+    }
+    GLuint Get(size_t idx = 0)
+    {
+        if (IDs.size() > idx)
+            return IDs[idx];
+        return 0;
+    }
+    std::vector<GLuint> IDs;
+};
+
+struct SBuffer
+{
+    SBuffer(GLenum type = GL_ARRAY_BUFFER, size_t size = 1)
+    {
+        this->type = type;
+        IDs.resize(size);
+        glGenBuffers(static_cast<GLsizei>(size), IDs.data());
+    }
+    ~SBuffer()
+    {
+        glDeleteBuffers(static_cast<GLsizei>(IDs.size()), IDs.data());
+    }
+    void Bind(size_t idx = 0)
+    {
+        if (idx < IDs.size())
+            glBindBuffer(type, IDs[idx]);
+    }
+    void UnBind()
+    {
+        glBindBuffer(type, 0);
+    }
+    GLuint Get(size_t idx = 0)
+    {
+        if (IDs.size() > idx)
+            return IDs[idx];
+        return 0;
+    }
+    std::vector<GLuint> IDs;
+    GLenum type;
+};
+
+struct STexture
+{
+    STexture(GLenum type = GL_TEXTURE_2D, size_t size = 1)
+    {
+        this->type = type;
+        IDs.resize(size);
+        glGenTextures(static_cast<GLsizei>(size), IDs.data());
+    }
+    ~STexture()
+    {
+        glDeleteTextures(static_cast<GLsizei>(IDs.size()), IDs.data());
+    }
+    void Bind(size_t idx = 0)
+    {
+        if (idx < IDs.size())
+            glBindTexture(type, IDs[idx]);
+    }
+    void UnBind()
+    {
+        glBindTexture(type, 0);
+    }
+    GLuint Get(size_t idx = 0)
+    {
+        if (IDs.size() > idx)
+            return IDs[idx];
+        return 0;
+    }
+    std::vector<GLuint> IDs;
+    GLenum type;
+};
+
+struct SFBO
+{
+    SFBO(size_t size = 1)
+    {
+        IDs.resize(size);
+        glGenFramebuffers(static_cast<GLsizei>(size), IDs.data());
+    }
+    ~SFBO()
+    {
+        glDeleteFramebuffers(static_cast<GLsizei>(IDs.size()), IDs.data());
+    }
+    void Bind(size_t idx = 0)
+    {
+        if (idx < IDs.size())
+            glBindFramebuffer(GL_FRAMEBUFFER, IDs[idx]);
+    }
+    void UnBind()
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+    GLuint Get(size_t idx = 0)
+    {
+        if (IDs.size() > idx)
+            return IDs[idx];
+        return 0;
+    }
+    std::vector<GLuint> IDs;
+};
+
+struct SRBO
+{
+    SRBO(size_t size = 1)
+    {
+        IDs.resize(size);
+        glGenRenderbuffers(static_cast<GLsizei>(size), IDs.data());
+    }
+    ~SRBO()
+    {
+        glDeleteRenderbuffers(static_cast<GLsizei>(IDs.size()), IDs.data());
+    }
+    void Bind(size_t idx = 0)
+    {
+        if (idx < IDs.size())
+            glBindRenderbuffer(GL_RENDERBUFFER, IDs[idx]);
+    }
+    void UnBind()
+    {
+        glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    }
+    GLuint Get(size_t idx = 0)
+    {
+        if (IDs.size() > idx)
+            return IDs[idx];
+        return 0;
+    }
+    std::vector<GLuint> IDs;
+};
+
+} // namespace RenderIt
